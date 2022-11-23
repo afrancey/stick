5aaed41c0e10aa1a5520258b44df8b39ad356274
afrancey <adam.francey.af@gmail.com>
2022-11-22 21:41:02 -0500

    comms between laptop and phone

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 // started 8:50pm nov 22
 // https://gamedevacademy.org/create-a-basic-multiplayer-game-in-phaser-3-with-socket-io-part-1/
 
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-app.use(express.static(__dirname + '/public'));
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+// var express = require('express');
+// var app = express();
+// var server = require('http').Server(app);
+// app.use(express.static(__dirname + '/public'));
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+// server.listen(8080, "0.0.0.0", function () {
+//   console.log(`Listening on ${server.address().port}`);
+// });
+
+
+
+// THIS WORKS FROM PHONE
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('Hello world')
 });
-server.listen(8080, "0.0.0.0", function () {
-  console.log(`Listening on ${server.address().port}`);
+
+server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}`)
 });
